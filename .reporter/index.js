bd000f8d8646dbd802fab593c20fb7aca50a08c3
Oscar Cantor <ocantorb1889@gmail.com>
2022-11-24 00:48:40 +0100

    First commit

--- .reporter/index.js
+++ .reporter/index.js
@@ -1,4 +1,4 @@
-const gitRemoteOriginUrl = require("git-remote-origin-url");
+const gitRemoteOriginUrl1 = require("git-remote-origin-url");
 const GitUrlParse = require("git-url-parse");
 const fs = require('fs');
 const os = require('os');
