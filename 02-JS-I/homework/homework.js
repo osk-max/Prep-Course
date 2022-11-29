3e46c77915fb8674640851da700fbb614dbd2322
Oscar Cantor <ocantorb1889@gmail.com>
2022-11-29 01:37:25 +0100

    Se crea un cuarto commit dejando valores de fabrica homework JS-I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,25 +1,22 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'hOLA mUNDO';
+const nuevaString = null;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 1263;
+const nuevoNum = null;
 
 // Crea una variable booleana:
-const nuevoBool = true;
+const nuevoBool = null;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
-nuevaResta = false;
 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * null === 40 ;
-nuevaMultiplicacion = false;
 
 // Resuelve el siguiente problema matemático:
 const nuevoModulo = 21 % 5 === null;
-nuevaModulo = false;
 
 
 // En los próximos 22 problemas, deberás completar la función.
